40dd003b17d6a2ffe34d7633064d83d81aa14bbd
wujiangang <wujg3@chinatelecom.cn>
2024-11-06 15:16:55 +0800

    init

---
+++ src/test/java/hsbc/incident/IncidentApplicationTests.java
@@ -0,0 +1,13 @@
+package hsbc.incident;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
