ed0868f18aacf36038cbaa821b5ee691ae0909d6
wujiangang <wujg3@chinatelecom.cn>
2024-11-07 21:25:44 +0800

    add stress test

---
+++ src/test/java/hsbc/incident/StressTest.java
@@ -0,0 +1,87 @@
+package hsbc.incident;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import hsbc.incident.common.Constants;
+import hsbc.incident.vo.IncidentVO;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class StressTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @SneakyThrows
+    public void performStressTest() {
+        // first the logic of testAPI() is correct
+        testAPI();
+
+        // then perform the stress test
+        int concurrency = 2000;
+        ExecutorService executor = Executors.newFixedThreadPool(concurrency);
+        for (int i = 0; i < concurrency; i++) {
+            executor.submit(this::testAPI);
+        }
+        executor.shutdown();
+    }
+
+    @SneakyThrows
+    private void testAPI() {
+        // create
+        IncidentVO incidentVO = new IncidentVO();
+        incidentVO.setDescription("description");
+        incidentVO.setStatus("PENDING");
+        MvcResult result = mockMvc.perform(post("/api/incidents")
+                        .content(toJsonString(incidentVO))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+        Long id = JsonPath.read(result.getResponse().getContentAsString(), "$.data");
+
+        // get
+        mockMvc.perform(get("/api/incidents/{id}", id))
+                .andExpect(jsonPath("$.statusCode").value(Constants.RESPONSE_CODE_SUCCESS))
+                .andExpect(jsonPath("$.data").isMap());
+
+        // list
+        mockMvc.perform(get("/api/incidents"))
+                .andExpect(jsonPath("$.statusCode").value(Constants.RESPONSE_CODE_SUCCESS));
+
+        // update
+        incidentVO.setDescription("modified description");
+        incidentVO.setStatus("FINISHED");
+        mockMvc.perform(put("/api/incidents/{id}", id)
+                        .content(toJsonString(incidentVO))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.statusCode").value(Constants.RESPONSE_CODE_SUCCESS))
+                .andExpect(jsonPath("$.data").value(true));
+
+        // delete
+        mockMvc.perform(delete("/api/incidents/{id}", id))
+                .andExpect(jsonPath("$.statusCode").value(Constants.RESPONSE_CODE_SUCCESS))
+                .andExpect(jsonPath("$.data").value(true));
+    }
+
+    public static String toJsonString(Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/test/java/hsbc/incident/UnitTest.java
+++ src/test/java/hsbc/incident/UnitTest.java
@@ -1,7 +1,6 @@
 package hsbc.incident;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jayway.jsonpath.JsonPath;
 import hsbc.incident.common.Constants;
 import hsbc.incident.entity.Incident;
 import hsbc.incident.mapper.IncidentMapper;
@@ -14,7 +13,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasSize;
@@ -23,7 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class ControllerTests {
+class UnitTest {
 
     @Autowired
     IncidentMapper incidentMapper;
@@ -176,69 +174,5 @@ class ControllerTests {
                 .andExpect(jsonPath("$.statusCode").value(Constants.RESPONSE_CODE_INCIDENT_NOT_FOUND))
                 .andExpect(jsonPath("$.data").isEmpty())
                 .andExpect(jsonPath("$.message").value("incident id -1 not found"));
-    }
-
-    /*
-     * Integration Testing
-     */
-    @Test
-    void whenPerformIntegrationTestAllApi_shouldSucceed() throws Exception {
-        // list all 3 incidents
-        mockMvc.perform(get("/api/incidents"))
-                .andExpect(jsonPath("$.statusCode").value(Constants.RESPONSE_CODE_SUCCESS))
-                .andExpect(jsonPath("$.data.records", hasSize(3)));
-
-        // create an incident
-        IncidentVO incidentVO = new IncidentVO();
-        incidentVO.setDescription("description");
-        incidentVO.setStatus("PENDING");
-        MvcResult result =  mockMvc.perform(post("/api/incidents")
-                        .content(toJsonString(incidentVO))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.statusCode").value(Constants.RESPONSE_CODE_SUCCESS))
-                .andExpect(jsonPath("$.data").isNumber())
-                .andReturn();
-        Long id = JsonPath.read(result.getResponse().getContentAsString(), "$.data");
-
-        // get the created incident
-        mockMvc.perform(get("/api/incidents/{id}", id))
-                .andExpect(jsonPath("$.statusCode").value(Constants.RESPONSE_CODE_SUCCESS))
-                .andExpect(jsonPath("$.data").isMap());
-
-        // list all 4 incidents
-        mockMvc.perform(get("/api/incidents"))
-                .andExpect(jsonPath("$.statusCode").value(Constants.RESPONSE_CODE_SUCCESS))
-                .andExpect(jsonPath("$.data.records", hasSize(4)));
-
-        // update the incident
-        incidentVO.setDescription("modified description");
-        incidentVO.setStatus("FINISHED");
-        mockMvc.perform(put("/api/incidents/{id}", id)
-                        .content(toJsonString(incidentVO))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$.statusCode").value(Constants.RESPONSE_CODE_SUCCESS))
-                .andExpect(jsonPath("$.data").value(true));
-
-        // verify the update
-        mockMvc.perform(get("/api/incidents/{id}", id))
-                .andExpect(jsonPath("$.statusCode").value(Constants.RESPONSE_CODE_SUCCESS))
-                .andExpect(jsonPath("$.data.description").value("modified description"))
-                .andExpect(jsonPath("$.data.status").value("FINISHED"));
-
-        // delete the incident
-        mockMvc.perform(delete("/api/incidents/{id}", id))
-                .andExpect(jsonPath("$.statusCode").value(Constants.RESPONSE_CODE_SUCCESS))
-                .andExpect(jsonPath("$.data").value(true));
-
-        // verify the deletion
-        mockMvc.perform(get("/api/incidents/{id}", id))
-                .andExpect(jsonPath("$.statusCode").value(Constants.RESPONSE_CODE_INCIDENT_NOT_FOUND))
-                .andExpect(jsonPath("$.data").isEmpty())
-                .andExpect(jsonPath("$.message").value(String.format("incident id %s not found", id)));
-
-        // verify the number of incidents is 3
-        mockMvc.perform(get("/api/incidents"))
-                .andExpect(jsonPath("$.statusCode").value(Constants.RESPONSE_CODE_SUCCESS))
-                .andExpect(jsonPath("$.data.records", hasSize(3)));
     }
 }
